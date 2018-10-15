4dc73d9029be6a2ae2f652c9e16dc2388586ee70
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2018-10-15 23:57:35 +0300

    Add noise to input image
    Pixel changes color with probability that is equal to `epsilon`.
    `epsilon` is known.
    Pixel doesn't change color with probability `1 - epsilon`.

---
+++ h2_gibbs_sampler/src/noise.rs
@@ -0,0 +1,28 @@
+pub mod noise {
+    extern crate rand;
+    use rand::Rng;
+    
+    pub fn noise_image(image: Vec<Vec<u32>>, epsilon: f64) -> Vec<Vec<u32>> {
+        let mut noised_image = vec![vec![0u32; ::WIDTH]; ::HEIGHT];
+        for i in 0..::HEIGHT {
+            for j in 0..::WIDTH {
+                if rand::thread_rng().gen_range(0., 1.) < epsilon {
+                    if image[i][j] == 0 {
+                        noised_image[i][j] = 1;
+                    } else {
+                        noised_image[i][j] = 0;
+                    }
+                } else {
+                    if image[i][j] == 1 {
+                        noised_image[i][j] = 1;
+                    }
+                }
+            }
+        }
+        println!("Noised image:");
+        for row in &noised_image {
+            println!("{:?}", row);
+        }
+        return noised_image;
+    }
+}
