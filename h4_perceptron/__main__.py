9383295264b55e7fc7f92b99aa34282cf1fedb97
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2018-11-28 00:02:09 +0200

    Add lists for dots
    2 lists for dots were added:
    - `dots1` for blue dots
    - `dots2` for red dots
    After drawing a dot, it is appended to a relevant list.
    
    These lists will be used as a set to traina perceptron to
    separate differnt types of dots with an ellipse.
    
    How to work with lists (or arrays):
    https://www.w3schools.com/python/python_arrays.asp

--- h4_perceptron/__main__.py
+++ h4_perceptron/__main__.py
@@ -3,16 +3,21 @@ from sys import stdout
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_xlim([0, 10])
-ax.set_ylim([0, 10])
+ax.set_xlim([-10, 10])
+ax.set_ylim([-10, 10])
+
+dots1 = []
+dots2 = []
 
 def on_click(event):
     print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
           (event.button, event.x, event.y, event.xdata, event.ydata))
     if event.button == 1:
         plt.plot(event.xdata, event.ydata, 'bo')
+        dots1.append([event.xdata, event.ydata])
     else:
         plt.plot(event.xdata, event.ydata, 'rx')
+        dots2.append([event.xdata, event.ydata])
     fig.canvas.draw()
 
 def on_press(event):
