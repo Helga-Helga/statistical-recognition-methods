ec214ea050a2b0f876c8aa7bafdd7341f3679af7
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2018-11-27 23:14:13 +0200

    Implement point drawing
    There are dots of 2 types:
    - inside an ellipse
    - outside an ellipse
    
    Dots of one type are created by left click of mouse.
    They are blue circles
    
    Dots of other type are created by right click of mouse.
    They are red crosses
    
    When user decides to spot drawing, he/she should press `c`.
    Then he/she is required to enter maximum number of minutes that
    he/she can wait to obtain a result (see an ellipse that divides different types of dots).
    Time is needed if input dots are not separated by an ellipse.
    Then the program should stop after after some iterations
    when suggested time is up.
    
    The idea how to plot a dot by mouse click:
    https://stackoverflow.com/questions/41824662/how-to-plot-a-dot-each-time-at-the-point-the-mouse-is-clicked-in-matplotlib
    
    How to draw a dot with different colors and markers:
    https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html
    
    Handling keyboard events:
    https://matplotlib.org/examples/event_handling/keypress_demo.html

---
+++ h4_perceptron/__main__.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from sys import stdout
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim([0, 10])
+ax.set_ylim([0, 10])
+
+def on_click(event):
+    print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+          (event.button, event.x, event.y, event.xdata, event.ydata))
+    if event.button == 1:
+        plt.plot(event.xdata, event.ydata, 'bo')
+    else:
+        plt.plot(event.xdata, event.ydata, 'rx')
+    fig.canvas.draw()
+
+def on_press(event):
+    print('key=%s' % event.key)
+    stdout.flush()
+    if event.key == 'c':
+        plt.clf()
+        fig.canvas.mpl_disconnect(on_click)
+        fig.canvas.mpl_disconnect(on_press)
+        wait_time = input("How long are you willing to wait? Enter the number of minutes...")
+
+
+cid_click = fig.canvas.mpl_connect('button_press_event', on_click)
+cid_press = fig.canvas.mpl_connect('key_press_event', on_press)
+
+plt.show()
