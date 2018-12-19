cd331c63b3da8b7a5c962beca3c77acc75a8471b
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2018-12-19 11:38:08 +0200

    Add comments

--- h5_em/__main__.py
+++ h5_em/__main__.py
@@ -1,24 +1,26 @@
-# MIT License
-#
-# Copyright (c) 2018 Olga Laviagina
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+"""
+MIT License
+
+Copyright (c) 2018 Olga Laviagina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 from matplotlib import pyplot
 from matplotlib.animation import FuncAnimation
 from numpy import random, linspace, zeros_like, array, zeros
@@ -27,16 +29,28 @@ from em import gaussian, em_step, likelihood
 fig = pyplot.figure()
 ax = fig.add_subplot(111)
 
+# Generate points of two classes with normal distribution with random parameters
 group1 = (random.randn(random.randint(10, 100)) * (1 + 10 * random.rand() ** 2) +
     random.rand() * 10).tolist()
 group2 = (random.randn(random.randint(10, 100)) * (1 + 10 * random.rand() ** 2) +
     random.rand() * 10).tolist()
-
 points = group1 + group2
+
+"""
+Generate random probabilities for each point to belong to each class:
+`alphas[0]` is a probability of each point to belong to the first class
+`alphas[1]` is a probability of each point to belong to the second class
+"""
 alphas = [random.rand(len(points))]
 alphas.append(1 - alphas[0])
 
 bins = linspace(-30, 30, 100)
+"""
+For each point `colors` contains a list with length 3, where
+the first element is a probability that this point belongs to the first class (red channel),
+the second element is zero (green channel) and
+the third element is a probability that this point belongs to the third class (blue channel).
+"""
 colors = zeros((len(points), 3), dtype="float64")
 colors[:, 0] = alphas[0]
 colors[:, 2] = alphas[1]
@@ -44,6 +58,11 @@ pyplot.hist(points, bins, alpha=0.3)
 pyplot.scatter(points, zeros_like(points) + 0.25, c=colors, marker='|', alpha=0.75, s=200)
 
 def animate(i):
+    """
+    Each time the function is called,
+    all probabilities and parameters are updated in `em_step`
+    and dots are redrawn with updated colors
+    """
     ax.clear()
     global alphas, q, mu, sigma
     alphas, q, mu, sigma = em_step(points, alphas)
--- h5_em/em.py
+++ h5_em/em.py
@@ -1,31 +1,47 @@
-# MIT License
-#
-# Copyright (c) 2018 Olga Laviagina
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+"""
+MIT License
+
+Copyright (c) 2018 Olga Laviagina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 from numpy import sqrt, pi, exp, zeros, array, log
 from itertools import compress
 
 def gaussian(x, mu, sigma):
+    """
+    x: value of a random variable
+    mu: expected value
+    sigma: variance
+    Returns a value of normal distribution density
+    """
     return 1. / (sqrt(2 * pi * sigma)) * exp( -(x - mu)**2 / (2 * sigma))
 
-def update_alphas(q, points, mu, sigma, alphas):
+def update_alphas(q, points, mu, sigma):
+    """
+    q: classes probabilities
+    points: random points of two classes
+    mu: expected value
+    sigma: variance
+    Returns updated probabilities for each point to belong to each class
+    """
+    alphas = zeros((2, len(points)))
     for i in range(len(points)):
         f0 = gaussian(points[i], mu[0], sigma[0])
         f1 = gaussian(points[i], mu[1], sigma[1])
@@ -34,12 +50,21 @@ def update_alphas(q, points, mu, sigma, alphas):
     return alphas
 
 def update_q(alphas):
+    """
+    alphas: probabilities for each point to belong to each class
+    Returns updated probabilities of classes
+    """
     q = array([0., 0.])
     q[0] = sum(alphas[0]) / len(alphas[0])
     q[1] = sum(alphas[1]) / len(alphas[1])
     return q
 
 def update_mu_sigma(alphas, points):
+    """
+    alphas: probabilities for each point to belong to each class
+    points: random points of two classes
+    Returns updated parameters of normal distributions
+    """
     mu = array([0., 0.])
     sigma = array([0., 0.])
     mu[0] = sum(alphas[0] * points) / sum(alphas[0])
@@ -49,12 +74,24 @@ def update_mu_sigma(alphas, points):
     return mu, sigma
 
 def em_step(points, alphas):
+    """
+    points: random points of two classes
+    alphas: probabilities for each point to belong to each class
+    Returns updated probabilities and parameters
+    """
     q = update_q(alphas)
     mu, sigma = update_mu_sigma(alphas, points)
-    alphas = update_alphas(q, points, mu, sigma, alphas)
+    alphas = update_alphas(q, points, mu, sigma)
     return alphas, q, mu, sigma
 
 def likelihood(q, points, mu, sigma):
+    """
+    q: classes probabilities
+    points: random points of two classes
+    mu: expected value
+    sigma: variance
+    Prints a value of likelihood
+    """
     res = 0
     for i in range(len(points)):
         res += log(q[0] * gaussian(points[i], mu[0], sigma[0]) +
